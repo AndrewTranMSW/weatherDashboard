59a6b691c3cf984a7f033a016a0dde756f147fc4
Andrew Tran <andrewtranmsw@gmail.com>
2022-01-11 20:38:59 -0800

    HTML done, starting on JS now

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var a = "hello world";
-console.log(a);
+let weatherRequestUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const APIKEY = "93e98dce2fd02f35575b27981d3da096"
+
+var timeText = document.querySelector('#timetext');
+var searchButton = document.querySelector('#searchBtn');
+var cityName = document.querySelector('#City');
+var currentWeatherIcon = document.querySelector('.currentWeather');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
+var uv = document.querySelector('#uv-index');
+var card = document.querySelector('.card');
+
+
+var a = 2 + 2
+
+console.log(a)
