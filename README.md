56aedf590ed83f13e3ca3e8ee1c036ee55577a3e
Andrew Tran <andrewtranmsw@gmail.com>
2022-01-11 23:06:10 -0800

    Main container functioning, README completed with current status of project

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # weatherDashboard
+A weather tracking application that utilizes JavaScript to fetch data from the Open Weater Map API.
+# Description
+The Weather Dashboard is a weather tracker where users are able to enter a city name and weather data regarding the city name, temperature, wind speed, humidity, and UV index will populate in the center. It fetches data from the Open Weather Map API that brings in real-time data.
+
+# To be added
+At this time, the 5 day-forecast and the previous city function are not working and I will need to finalize this function.
+
+MomentJS will be added to provide real-time date and time information as well.
+
+# Languages Utilized
+- HTML
+- CSS
+- JavaScript
+
+# Assets Utilized
+- jQuery
+- Bootstrap
+
+# API 
+- https://openweathermap.org/api
+
+# Images 
+! [A screen capture of the website populating the current weather data for Chicago, IL](./assets/Capture.JPG)
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+# Credits
+- https://openweathermap.org/appid
--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 let weatherRequestUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const APIKEY = "93e98dce2fd02f35575b27981d3da096"
 
-var timeText = document.querySelector('#timetext');
-var searchButton = document.querySelector('#searchBtn');
-var cityName = document.querySelector('#City');
-var currentWeatherIcon = document.querySelector('.currentWeather');
-var temp = document.querySelector('#temp');
-var wind = document.querySelector('#wind');
-var humidity = document.querySelector('#humidity');
-var uv = document.querySelector('#uv-index');
-var card = document.querySelector('.card');
+//Global? Nope. Not today.
+// var cityInfo = document.getElementById(('city'))
+// var tempInfo = document.getElementById(('temp'))
+// var windInfo = document.getElementById(('wind'))
+// var humInfo = document.getElementById(('humidity'))
+// var uvInfo = document.getElementById(('uv'))
 
+function citySearch() {
+    var searchInput = document.getElementById('citySearch'.trim())
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=93e98dce2fd02f35575b27981d3da096')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    weatherStats(data);
+  });
+}
 
-var a = 2 + 2
+function weatherStats(response) {
+    console.log(response)
 
-console.log(a)
+    var cityInfo = document.getElementById(('city'))
+    var tempInfo = document.getElementById(('temp'))
+    var windInfo = document.getElementById(('wind'))
+    var humInfo = document.getElementById(('humidity'))
+    var uvInfo = document.getElementById(('uv'))
+
+    var todayDate = new Date()
+    var month = todayDate.getMonth() + 1
+    var day = todayDate.getDate();
+    var year = todayDate.getFullYear();
+
+    cityInfo.innerText = response.name + " " + month + "/" + day + "/" + year
+    tempInfo.innerHTML = "Temperature: " + Math.floor(((response.main.temp-273.15)*1.8)+32) + "&#8457;"
+    windInfo.innerText = "Wind Speed: " + response.wind.speed
+    humInfo.innerText = "Humidity: " + response.main.humidity
+    uvInfo.innerText = "UV: " + response
+}
